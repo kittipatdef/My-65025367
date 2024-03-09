94ebefbaf4a411db452303552738466bae8d7146
kittipatdef <65025367@up.ac.th>
2024-03-09 16:59:57 +0700

    first commit

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Skills.module.css'
+import { FaSquareJs, FaReact, FaCss3 , FaGitAlt, FaNode, FaPython, FaC, FaCableCar, FaJava} from 'react-icons/fa6'
+
+function Skills() {
+  return (
+    <div className={styles.skills_container}>
+        <h3 className={styles.skills_title}>My Skills Set</h3>
+        <ul className={styles.skills_list}>
+            <li><FaSquareJs/></li>
+            <li><FaReact/></li>
+            <li><FaCss3 /></li>
+            <li><FaGitAlt/></li>
+            <li><FaNode/></li>
+            <li><FaPython/></li>
+            <li><FaJava/></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Skills
